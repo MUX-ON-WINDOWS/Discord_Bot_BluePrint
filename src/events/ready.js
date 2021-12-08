ae6cd203d0cd1c1c2e35e57ab307e7b64031c353
Max <max.arnouts@ictmbo.nl>
2021-12-08 11:05:59 +0100

    Added activity status

--- src/events/ready.js
+++ src/events/ready.js
@@ -3,5 +3,7 @@ module.exports = {
 	once: true,
 	async execute(client) {
 		console.log(`Your bot is now online! Logged in as ${client.user.tag}`);
+		//Playing	0, Streaming   1 Listening	2, Watching	3, Custom	4, Competing	5,
+		client.user.setPresence({ activities: [{ type: `WATCHING`, name: `Muxic`, url:`https://maxarnouts.nl`}], status: 'dnd', afk: false});
 	},
 };
