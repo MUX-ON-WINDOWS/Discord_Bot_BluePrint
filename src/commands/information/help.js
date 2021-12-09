5a460344a48a07210d34b96d038a83913fa56488
Max <max.arnouts@ictmbo.nl>
2021-12-09 14:05:17 +0100

    Updated API and added a Help command (overview of all commands)

--- src/commands/information/help.js
+++ src/commands/information/help.js
@@ -1,10 +1,69 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
+const {
+      SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+      MessageEmbed
+} = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('help')
-		.setDescription('Returns an overview of the commands'),
-	async execute(interaction) {
-		await interaction.reply('');
-	},
+      data: new SlashCommandBuilder()
+            .setName('help')
+            .setDescription('"Help you to get all possible commands')
+            .addSubcommand(subcommand =>
+                  subcommand
+                  .setName("commands")
+                  .setDescription("Information about the server commands.")),
+      async execute(interaction, client) {
+            if (interaction.options.getSubcommand() === "commands") {
+                  const helpEmbed = new MessageEmbed()
+                        .setAuthor(`${client.user.tag}`, client.user.displayAvatarURL())
+                        .setTitle(`Server commands:`)
+                        .addFields({
+                              name: 'Command:',
+                              value: `/info server`,
+                              inline: true
+                        }, {
+                              name: 'Command ',
+                              value: `/info user @...`,
+                              inline: true
+                        }, {
+                              name: 'Command:',
+                              value: `/ping`,
+                              inline: true
+                        }, {
+                              name: 'Command ',
+                              value: `empty/ not yet used`,
+                              inline: true
+                        }, {
+                              name: 'Command:',
+                              value: `empty/ not yet used`,
+                              inline: true
+                        }, {
+                              name: 'Command ',
+                              value: `empty/ not yet used`,
+                              inline: true
+                        }, {
+                              name: 'Command:',
+                              value: `empty/ not yet used`,
+                              inline: true
+                        }, {
+                              name: 'Command ',
+                              value: `empty/ not yet used`,
+                              inline: true
+                        }, {
+                              name: 'Command:',
+                              value: `empty/ not yet used`,
+                              inline: true
+                        }, )
+                        .setTimestamp()
+                        .setColor("RANDOM")
+                        .setFooter(`${client.user.tag}`, client.user.displayAvatarURL());
+                  //Reply the embed design
+                  await interaction.reply({
+                        embeds: [helpEmbed]
+                  });
+            } else {
+                  await interaction.reply(`No command found`);
+            }
+      }
 };
--- src/commands/information/info.js
+++ src/commands/information/info.js
@@ -1,21 +1,26 @@
-const { SlashCommandBuilder } = require('@discordjs/builders');
-const { MessageEmbed, MessageAttachment } = require('discord.js');
+const {
+    SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+    MessageEmbed,
+    MessageAttachment
+} = require('discord.js');
 
 module.exports = {
-	data: new SlashCommandBuilder()
-		.setName('info')
-		.setDescription('Returns info based on input')
+    data: new SlashCommandBuilder()
+        .setName('info')
+        .setDescription('Returns info based on input')
         .addSubcommand(subcommand =>
             subcommand
-                .setName("user")
-                .setDescription("Get information about the user you mention.")
-                .addUserOption(option => option.setName("target").setDescription("@ name you want to metion")))
+            .setName("user")
+            .setDescription("Get information about the user you mention.")
+            .addUserOption(option => option.setName("target").setDescription("@ name you want to metion")))
         .addSubcommand(subcommand =>
             subcommand
-                .setName("server")
-                .setDescription("Information about this server.")),
-	async execute(interaction, client) {
-		if (interaction.options.getSubcommand() === "user") {
+            .setName("server")
+            .setDescription("Information about this server.")),
+    async execute(interaction, client) {
+        if (interaction.options.getSubcommand() === "user") {
             const user = interaction.options.getUser("target");
             if (user) {
                 // Image will be used here in your embed will be called at setImage()
@@ -23,30 +28,64 @@ module.exports = {
                 const userEmbed = new MessageEmbed()
                     .setTitle(`${user.username}'s information: `)
                     .setDescription("This is the information you wanted to know.")
-                    .setURL("https://maxarnouts.nl")
                     // .setAuthor("MUX_ON_WINDOWS", client.user.displayAvatarURL(),"https://maxarnouts.nl")
-
-                    .setAuthor("MUX_ON_WINDOWS", client.user.displayAvatarURL())
+                    .setAuthor(`${client.user.tag}`, client.user.displayAvatarURL())
                     .setThumbnail(client.user.displayAvatarURL())
                     .addFields(
-                        { name: '\u200B', value: '\u200B' },
-                        { name: 'Username:', value: `Username is: ${user.username}`, inline: true },
-                        { name: 'Tag:', value: `Tag is: #${user.discriminator}`, inline: true },
+                        // create empty space.
+                        {
+                            name: '\u200B',
+                            value: '\u200B'
+                        },
+                        // gets username.
+                        {
+                            name: 'Username:',
+                            value: `Username is: ${user.username}`,
+                            inline: true
+                        },
+                        // #tag discord.
+                        {
+                            name: 'Tag:',
+                            value: `Tag is: #${user.discriminator}`,
+                            inline: true
+                        },
                     )
                     .setImage("attachment://bot.jpg")
                     .setTimestamp()
-                    .setColor("DARKER_GREY")
-                    .setFooter(client.user.tag, client.user.displayAvatarURL());
-                    //Reply the embed design
-                await interaction.reply({embeds: [userEmbed], files: [file]});
+                    .setColor("RANDOM")
+                    .setFooter(`${client.user.tag}`, client.user.displayAvatarURL());
+                //Reply the embed design
+                await interaction.reply({
+                    embeds: [userEmbed],
+                    files: [file]
+                });
 
             } else {
                 await interaction.reply(`Username: ${interaction.user.username}\nYour user ID: ${interaction.user.id}`);
             }
         } else if (interaction.options.getSubcommand() === "server") {
-            await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+            const serverEmbed = new MessageEmbed()
+                .setAuthor(`${client.user.tag}`, client.user.displayAvatarURL())
+                .setTitle(`Server information:`)
+                .addFields({
+                    name: 'Server name:',
+                    value: `${interaction.guild.name}`,
+                    inline: true
+                }, {
+                    name: 'Total members: ',
+                    value: `${interaction.guild.memberCount}`,
+                    inline: true
+                }, )
+                .setTimestamp()
+                .setColor("RANDOM")
+                .setFooter(`${client.user.tag}`, client.user.displayAvatarURL());
+            //Reply the embed design
+            await interaction.reply({
+                embeds: [serverEmbed]
+            });
+
         } else {
-            await interaction.reply("No command used, try again!");
+            await interaction.reply(`Command didn't work please contact the owner of the bot`);
         }
-	},
+    },
 };
